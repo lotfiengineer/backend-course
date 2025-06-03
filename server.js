101a4670e43ec8fb967d2ea76b514c45d860dc3d
Mohammad Lotfi <mohammad.lotfi7913@gmail.com>
2025-06-03 14:42:36 +0330

    implement catch all not found page

--- server.js
+++ server.js
@@ -20,8 +20,15 @@ app.use("/", require("./routes/root"));
 app.use("/subdir", require("./routes/subdir"));
 app.use("/employees", require("./routes/api/employees"));
 
-app.get(/\/*/, (req, res) => {
-  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+app.all(/\/*/, (req, res) => {
+  res.status(404);
+  if (req.accepts("html")) {
+    res.sendFile(path.join(__dirname, "views", "404.html"));
+  } else if (req.accepts("json")) {
+    res.json({ error: "404 Not Found" });
+  } else {
+    res.type("txt").send("404 Not Found");
+  }
 });
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
